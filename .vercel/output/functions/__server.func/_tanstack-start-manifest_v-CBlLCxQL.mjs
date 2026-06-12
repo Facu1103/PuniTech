4c5ac6f39f307d1a82e9bd495e936332ff29a0f9
Facu Guarnieri <guarnierifacu15@gmail.com>
2026-06-12 14:04:16 -0300

    Revert "Corregir build de producción para Vercel"
    This reverts commit 45a7b0f843d6febcd35319572f34647cf281f42c.

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CBlLCxQL.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CBlLCxQL.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/Guarn/Desktop/punilla-tech-hub-main/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-niFPhvkq.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-niFPhvkq.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/Guarn/Desktop/punilla-tech-hub-main/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-BD-xJvaq.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
